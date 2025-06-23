3988446c0639527b5d48868f819dce6986024a16
charleemagn <112896538+charleemagn@users.noreply.github.com>
2025-06-23 21:50:08 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Программный модуль обработки данных")
+title = tk.Label(text="Программный модуль обработки данных")
+title.pack()
+window.mainloop()
